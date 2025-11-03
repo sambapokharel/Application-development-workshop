a45f761400d05c074f2a18f465554e868a212703
sambapokharel <sambapokharel7@gmail.com>
2025-11-03 12:08:53 +0545

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Circle
+{
+    const double pi = 3.14;
+
+    public  static void Main()
+    {
+        Console.WriteLine(CalculateArea(5));
+        Console.WriteLine(CalculateCircumference(5));
+    }
+
+     public static double CalculateArea(double radius)
+    {
+        return pi * radius * radius;
+    }
+    public static double CalculateCircumference(double radius)
+    {
+        return 2 * pi * radius;
+    }
+}
